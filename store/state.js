bdc8983bc8a819c7c359875316698c983ac6078d
ZRothschild <873908960@qq.com>
2019-06-04 22:14:31 +0800

    state 使用

--- store/state.js
+++ store/state.js
@@ -1,16 +1,16 @@
-// export const state = () => ({
-//     siteIntro: [
-//         {
-//             intitle:'跨平台',
-//             intcont:'编写一次并在任何地方以最小的机器功率运行，如Android、ios、Linux和Windows等。它支持Google Go，只需一个可执行的服务即可在所有平台。'
-//         },
-//         {
-//             intitle:'易于使用',
-//             intcont:' Iris以简单而强大的api而闻名。 除了Iris为您提供的低级访问权限。 Iris同样擅长MVC。 它是唯一一个拥有MVC架构模式丰富支持的Go Web框架，性能成本接近于零。'
-//         },
-//         {
-//             intitle:'面向服务',
-//             intcont:'Iris为您提供构建面向服务的应用程序的结构。 用Iris构建微服务很容易。'
-//         }
-//     ],
-// })
+export default () => ({
+    siteIntro: [
+        {
+            intitle:'跨平台',
+            intcont:'编写一次并在任何地方以最小的机器功率运行，如Android、ios、Linux和Windows等。它支持Google Go，只需一个可执行的服务即可在所有平台。'
+        },
+        {
+            intitle:'易于使用',
+            intcont:' Iris以简单而强大的api而闻名。 除了Iris为您提供的低级访问权限。 Iris同样擅长MVC。 它是唯一一个拥有MVC架构模式丰富支持的Go Web框架，性能成本接近于零。'
+        },
+        {
+            intitle:'面向服务',
+            intcont:'Iris为您提供构建面向服务的应用程序的结构。 用Iris构建微服务很容易。'
+        }
+    ],
+})
